4f88743fad21787b07ca267229c28e9175534286
impactlearner <time.darden@gmail.com>
2020-04-30 16:19:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,8 +18,18 @@ end
 
 def output_array(array)
 count = 0
-while count < array.length do
+  while count < array.length do
   puts array[count]
   count += 1
+  end
 end
+
+def return_string_array (array)
+  count = 0
+  new array = []
+  while count < array.length do
+    new_array.push(array[count].to_s)
+    count += 1
+  end
+  new_array
 end
