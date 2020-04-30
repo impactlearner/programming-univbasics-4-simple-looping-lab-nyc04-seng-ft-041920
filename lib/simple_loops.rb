f26b597e3d1da41339932900d0c92d129c58c436
impactlearner <time.darden@gmail.com>
2020-04-30 16:10:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,17 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  count = 0
+  while count <= 5 do
+    puts string
+    count += 1
+  end
+end
+
+def loop_message_n_times(string, limit)
+  count = 0
+  while count < limit do
+    puts string
+    count += 1
+  end
+end
